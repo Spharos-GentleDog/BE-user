8b70050ba76254ba215625e839cfb0e281627932
ycm0926 <yangyang09261@naver.com>
2023-10-25 17:40:00 +0900

    refactor: Allarg삭제

--- src/main/java/egenius/global/exception/BaseException.java
+++ src/main/java/egenius/global/exception/BaseException.java
@@ -1,11 +1,9 @@
 package egenius.global.exception;
 
 import egenius.global.base.BaseResponseStatus;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class BaseException extends Exception {
     private BaseResponseStatus status;
 
